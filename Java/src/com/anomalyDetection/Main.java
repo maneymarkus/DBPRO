36de6ed29b66aba386e0b43e1e1cc35647e0bd85
rkrys <rkrys@DESKTOP-0LM1DFK.fritz.box>
2020-05-12 11:24:20 +0200

    Added Segment and Sorter Class for utility and data management things

--- Java/src/com/anomalyDetection/Main.java
+++ Java/src/com/anomalyDetection/Main.java
@@ -7,10 +7,13 @@ public class Main {
     public static void main(String[] args) {
         String csvName = "a5.csv";
         String currentWorkingDir = System.getProperty("user.dir");
-        String pathToCsv = currentWorkingDir + "/../" + csvName;
+        String pathToCsv = currentWorkingDir +"/" + csvName; //+ "/../"
+        System.out.println(pathToCsv);
         Reader reader = new Reader(pathToCsv);
         //reader.readSomeLines();
         List<Line> readLines = reader.readLinesToObjects();
         System.out.println(readLines.get(450000).getDateTime());
+        
+        
     }
 }
